0e9ca45132b3a7f0a91c5b559e40314e31115755
pelesesenko <kw04@i.ua>
2018-12-21 08:47:53 +0300

    Защита 2

--- js/script.js
+++ js/script.js
@@ -1,95 +1,112 @@
 var mapModal = document.querySelector(".modal-map");
-var mapOpen = document.querySelector(".open-map");
-var mapClose = mapModal.querySelector(".modal-map .modal-close");
 var feedbackModal = document.querySelector(".feedback-modal");
-var feedbackOpen = document.querySelector(".btn-open-form");
-var feedbackClose = feedbackModal.querySelector(".feedback-modal .modal-close");
-var feedbackSubmit = feedbackModal.querySelector("[type=submit]");
-var feedbackLogin = feedbackModal.querySelector("[name=login]");
-var feedbackEmail = feedbackModal.querySelector("[name=email]");
-var feedbackTxtArea = feedbackModal.querySelector("[name=mail-text]");
-var overlay = document.querySelector(".overlay");
 
-var isStorageSupport = true;
-var storageLogin = "";
-var storageEmail = "";
+if (mapModal) {
+    var mapOpen = document.querySelector(".open-map");
+    var mapClose = mapModal.querySelector(".modal-map .modal-close");
+    var overlay = document.querySelector(".overlay");
 
-try {
-    storageLogin = localStorage.getItem("login");
-    storageEmail = localStorage.getItem("email");
-} catch (err) {
-    isStorageSupport = false;
+    mapOpen.addEventListener("click", function(evt) {
+        evt.preventDefault();
+        mapModal.classList.add("modal-show");
+        overlay.style.display = "block";
+    });
+
+    mapClose.addEventListener("click", function(evt) {
+        evt.preventDefault();
+        mapModal.classList.remove("modal-show");
+        overlay.style.display = "";
+    });
+
+    window.addEventListener("keydown", function(evt) {
+        if (evt.keyCode == 27) {
+            if (mapModal.classList.contains("modal-show")) {
+                evt.preventDefault();
+                mapModal.classList.remove("modal-show");
+                overlay.style.display = "";
+
+            }
+            if (feedbackModal.classList.contains("modal-show")) {
+                evt.preventDefault();
+                feedbackModal.classList.remove("modal-error");
+                feedbackModal.classList.remove("modal-show");
+                overlay.style.display = "";
+            }
+
+        }
+    });
 }
 
+if (feedbackModal) {
+    var feedbackOpen = document.querySelector(".btn-open-form");
+    var feedbackClose = feedbackModal.querySelector(".feedback-modal .modal-close");
+    var feedbackSubmit = feedbackModal.querySelector("[type=submit]");
+    var feedbackLogin = feedbackModal.querySelector("[name=login]");
+    var feedbackEmail = feedbackModal.querySelector("[name=email]");
+    var feedbackTxtArea = feedbackModal.querySelector("[name=mail-text]");
 
+    feedbackOpen.addEventListener("click", function(evt) {
+        evt.preventDefault();
+        feedbackModal.classList.add("modal-show");
+        overlay.style.display = "block";
+        if (storageLogin) {
+            feedbackLogin.value = storageLogin;
+            feedbackEmail.focus();
+            if (storageEmail) {
+                feedbackEmail.value = storageEmail;
+                feedbackTxtArea.focus();
+            }
 
-
-window.addEventListener("keydown", function(evt) {
-    if (evt.keyCode == 27) {
-        if (mapModal.classList.contains("modal-show")) {
-            evt.preventDefault();
-            mapModal.classList.remove("modal-show");
-            overlay.style.display = "";
-
-        }
-        if (feedbackModal.classList.contains("modal-show")) {
+        } else { feedbackLogin.focus() }
+    });
+    feedbackClose.addEventListener("click", function(evt) {
+        evt.preventDefault();
+        feedbackModal.classList.remove("modal-show");
+        feedbackModal.classList.remove("modal-error");
+        overlay.style.display = "";
+    });
+    feedbackSubmit.addEventListener("click", function(evt) {
+        if (!feedbackLogin.value || !feedbackEmail.value) {
             evt.preventDefault();
             feedbackModal.classList.remove("modal-error");
-            feedbackModal.classList.remove("modal-show");
-            overlay.style.display = "";
-        }
-
-    }
-});
-mapOpen.addEventListener("click", function(evt) {
-    evt.preventDefault();
-    mapModal.classList.add("modal-show");
-    overlay.style.display = "block";
-});
-feedbackOpen.addEventListener("click", function(evt) {
-    evt.preventDefault();
-    feedbackModal.classList.add("modal-show");
-    overlay.style.display = "block";
-    if (storageLogin) {
-        feedbackLogin.value = storageLogin;
-        feedbackEmail.focus();
-        if (storageEmail) {
-            feedbackEmail.value = storageEmail;
-            feedbackTxtArea.focus();
-        }
-
-    } else { feedbackLogin.focus() }
-});
-
-mapClose.addEventListener("click", function(evt) {
-    evt.preventDefault();
-    mapModal.classList.remove("modal-show");
-    overlay.style.display = "";
-});
-feedbackClose.addEventListener("click", function(evt) {
-    evt.preventDefault();
-    feedbackModal.classList.remove("modal-show");
-    feedbackModal.classList.remove("modal-error");
-    overlay.style.display = "";
-});
-feedbackSubmit.addEventListener("click", function(evt) {
-    if (!feedbackLogin.value || !feedbackEmail.value) {
-        evt.preventDefault();
-        feedbackModal.classList.remove("modal-error");
-        feedbackModal.offsetWidth = feedbackModal.offsetWidth;
-        feedbackModal.classList.add("modal-error");
-        feedbackLogin.style.backgroundColor = "#f6dada";
-        if (!feedbackLogin.value) {
+            feedbackModal.offsetWidth = feedbackModal.offsetWidth;
+            feedbackModal.classList.add("modal-error");
             feedbackLogin.style.backgroundColor = "#f6dada";
+            if (!feedbackLogin.value) {
+                feedbackLogin.style.backgroundColor = "#f6dada";
+            }
+            if (!feedbackEmail.value) {
+                feedbackEmail.style.backgroundColor = "#f6dada";
+            }
+        } else {
+            if (isStorageSupport) {
+                localStorage.setItem("feedbackLogin", feedbackLogin.value);
+                localStorage.setItem("feedbackEmail", feedbackEmail.value);
+            }
         }
-        if (!feedbackEmail.value) {
-            feedbackEmail.style.backgroundColor = "#f6dada";
-        }
-    } else {
-        if (isStorageSupport) {
-            localStorage.setItem("feedbackLogin", feedbackLogin.value);
-            localStorage.setItem("feedbackEmail", feedbackEmail.value);
-        }
-    }
 
+    });
+    var isStorageSupport = true;
+    var storageLogin = "";
+    var storageEmail = "";
+
+    try {
+        storageLogin = localStorage.getItem("login");
+        storageEmail = localStorage.getItem("email");
+    } catch (err) {
+        isStorageSupport = false;
+    }
+}
+
+var catalogMenu = document.querySelector(".header-catalog-menu");
+var catalogMenuAnchors = catalogMenu.querySelectorAll(".header-catalog-menu a");
+var goodsCatalog = document.querySelector(".goods-catalog");
+
+
+goodsCatalog.addEventListener("focus", function() {
+    catalogMenu.style.visibility = "visible";
 });
+
+catalogMenuAnchors[catalogMenuAnchors.length - 1].addEventListener("blur", function() {
+    catalogMenu.style.visibility = "";
+})
